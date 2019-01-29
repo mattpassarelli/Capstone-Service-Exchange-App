cecaf815eecfa0a7a76031d1922450c76405553b
Matt Passarelli <mattvpassarelli@gmail.com>
2019-01-28 23:03:10 -0500

    Added the initial dropdown picker for requests. Got this awesome Node Module for responsive text scaling as well

--- Screens/NewRequest.js
+++ Screens/NewRequest.js
@@ -1,12 +1,14 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Picker } from 'react-native';
-
+import React, { Component, PureComponent } from 'react';
+import { View, Text, StyleSheet, ScrollView, } from 'react-native';
+import Picker from 'react-native-universal-picker'
+import RF from "react-native-responsive-fontsize"
 
 // define your styles
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
     },
 });
 
@@ -20,36 +22,53 @@ const ScrollStyle = StyleSheet.create({
 
 
 // create a component
-class NewRequest extends Component {
+class NewRequest extends PureComponent {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props)
 
         this.state = {
-            selectedItem: "Select a Request"
+            selectedItem: "",
         }
     }
 
+    componentDidMount() {
+
+    }
+
+    handleValueChange = (itemValue, itemIndex) => {
+        this.setState({
+            selectedItem: itemValue,
+        })
+    }
+
+
     render() {
+
         return (
 
             <React.Fragment>
 
                 <View style={styles.container}>
 
-                    <ScrollView contentContainerStyle={ScrollStyle.container}>
-                    <Text>Pick one below</Text>
-                        <Picker 
-                        selectedValue={this.state.selectedItem}
-                        onValueChange={(itemValue, itemIndex) =>
-                        this.setState({selectedItem: itemValue})}
-                        style={{height: 40, width: 200}}
-                        >
-                        <Picker.Item label="option one" value="option one"/>
-                        <Picker.Item label="option two" value="option two"/>
-                        </Picker>
-                    </ScrollView>
+                    <View style={{flex: 1, flexDirection: "row", justifyContent: "space-around"}}>
+                        <Text style={{paddingTop:15, fontSize: RF(2.5)}}>Select a Request...</Text>
+                        
+                            <Picker
+                                selectedValue={this.state.selectedItem}
+                                onValueChange={this.handleValueChange}
+                                style={{ backgroundColor: "rgba(137, 132, 132, 0.1)" , height: 50, width: "50%", alignContent: 'center', justifyContent: "center"}}
+                                prompt="Select a Request Type"
+                            >
+
+                                <Picker.Item label="java" value="java" />
+                                <Picker.Item label="JS" value="js" />
+
+                            </Picker>
+                        
+                    </View>
+
+                    <View style={{}}></View>
                 </View>
             </React.Fragment>
         );
